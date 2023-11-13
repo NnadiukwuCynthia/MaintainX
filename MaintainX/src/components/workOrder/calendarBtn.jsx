484a5846092140a345708b472560d018a11a104f
Cynthia Nnadiuwku <cynthiaamaran@gmail.com>
2023-11-13 14:31:08 +0100

    task: Worked on work order header

---
+++ MaintainX/src/components/workOrder/calendarBtn.jsx
@@ -0,0 +1,15 @@
+// import {TiCalendarOutline} from 'react-icons/ti'
+// import Button from './Button';
+
+
+// const CalendarBtn = () => {
+//     // const HandleClick = () => {
+//     //     console.log('button clicked');
+//     // }
+//     return(
+    
+//     )
+    
+// }
+
+// export default CalendarBtn
--- MaintainX/src/components/workOrder/header.jsx
+++ MaintainX/src/components/workOrder/header.jsx
@@ -1,12 +1,18 @@
-import {GoColumns} from 'react-icons/go'
+import {GoColumns, GoTable} from 'react-icons/go'
+import {IoCalendarClearOutline} from 'react-icons/io5'
+import Button from '../Button';
+
 
 const Header = () => {
   return (
     <div className="header">
-       <div>
-       <h1 className="header__text">Work Orders</h1>
-        <GoColumns/>
-        
+       <div className='header__container'>
+          <h1 className="header__container__text">Work Orders</h1>
+          <div className='header__btnContainer'>
+            <Button className='headerBtn'> <GoColumns/></Button>
+            <Button className='headerBtn'><GoTable/></Button>
+            <Button className='headerBtn'><IoCalendarClearOutline/></Button>
+          </div>
        </div>
     </div>
   )
